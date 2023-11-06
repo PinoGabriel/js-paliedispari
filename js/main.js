27b07b8947f2ca005c63d0a6f132ec5f6f95c692
PinoGabriel <pinogabriel01@gmail.com>
2023-11-06 15:54:10 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+  
+  const userInput = prompt("Inserisci una parola:");
+  
+  for (let i = 0; i < userInput.length; i++) {
+    if (userInput[i] ) {
+      console.log(userInput[i]);
+    }
+  }
