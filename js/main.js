7bda310300c5378598efd81509359d0027a4c3f6
PinoGabriel <pinogabriel01@gmail.com>
2023-11-06 16:56:21 +0100

    feat: added function palindromo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
+let reverseWord = '';
+const textHTML = document.getElementById("palindroma")
 
-  
-  const userInput = prompt("Inserisci una parola:");
-  
-  for (let i = 0; i < userInput.length; i++) {
-    if (userInput[i] ) {
-      console.log(userInput[i]);
+function palindromo(word) {
+    
+    for (let i = word.length - 1; i>=0;  i--) {
+        reverseWord += word[i];
     }
-  }
+    
+
+    if (word == reverseWord) {
+        return true
+      } else {
+        return false
+      }
+}
+
+const userWord = prompt("Inserisci una parola:", "anna");
+let result = palindromo(userWord)
+  
+if (result) {
+    console.log("la parola è palindroma");
+    textHTML.innerHTML = `<h1> ${userWord} (è una parola palindroma) </h1>`
+    textHTML.classList.add("green")
+    textHTML.classList.remove("red")
+} else {
+    console.log("la parola NON è palindroma");
+    textHTML.innerHTML = `<h1> ${userWord} (NON è una parola palindroma) </h1>`
+    textHTML.classList.add("red")
+    textHTML.classList.remove("green")
+}
